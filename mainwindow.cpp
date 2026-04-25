fe7719d480fb3a5878e1297e8d3f214a1aa828d3
Sang <huynhtansang102006@gmail.com>
2026-04-25 20:43:56 +0700

    Thu nghiem tao cong theo chieu doc

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -194,13 +194,11 @@ void MainWindow::on_componentList_itemPressed(QListWidgetItem *item)
                 // Tính toán vị trí tâm của view và chuyển sang tọa độ scene
                 QPointF centerPos = currentView->mapToScene(currentView->viewport()->width() / 2,
                                                             currentView->viewport()->height() / 2);
-                static int offsetCount = 0; 
-                // Cộng thêm độ lệch theo bội số của gridSize (60)
-                centerPos.setX(centerPos.x() + (offsetCount * 60));
-                centerPos.setY(centerPos.y() + (offsetCount * 60));
                 
-                // Tăng biến đếm, nếu quá 5 thì reset về 0
-                offsetCount = (offsetCount + 1) % 5;
+                static int offsetCount = 0; 
+                centerPos.setY(centerPos.y() + (offsetCount * 60));
+                offsetCount = (offsetCount + 1) % 4;
+                
                 gate->setPos(centerPos);
                 setDocumentDirty(true);
             } else {
